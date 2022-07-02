0ef08eba4f848e54fca62de168a2edb3bf9127c8
bishal_giri <giribishal09@gmail.com>
2022-07-02 22:06:40 +0545

    swagger docs added

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import Express, { Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
 
 // imports for ----> (ROUTES)
 import UserRouter from "app/routes/user";
@@ -31,6 +33,31 @@ app.get("/", (req: Request, res: Response) => {
     value: "This is a homepage and a placeholder response (CD CHECK)..",
   });
 });
+
+// Swagger definition
+const swaggerDefinition = {
+  info: {
+    title: "REST API for Knowledge Seekers", // Title of the documentation
+    version: "1.0.0", // Version of the app
+    description:
+      "This is the REST API for our final year project Knowledge Seekers", // short description of the app
+  },
+  host: "https://ks-api.vercel.app", // the host or url of the app
+  basePath: "/api/v1", // the basepath of your endpoint
+};
+
+// options for the swagger docs
+const options = {
+  // import swaggerDefinitions
+  swaggerDefinition,
+  // path to the API docs
+  apis: ["./docs/**/*.yaml"],
+};
+// initialize swagger-jsdoc
+const swaggerSpec = swaggerJSDoc(options);
+
+// use swagger-Ui-express for your app documentation endpoint
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack);
