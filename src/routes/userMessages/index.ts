bae538b4e95d5611680e869f4d069e3cf1eeaf7e
bishal_giri <giribishal09@gmail.com>
2022-10-15 17:44:18 +0545

    delete route

--- src/routes/userMessages/index.ts
+++ src/routes/userMessages/index.ts
@@ -2,11 +2,13 @@ import passport from "passport";
 import Express from "express";
 import {
   createMessage,
+  deleteSingleMessage,
   getAllMessages,
 } from "app/controllers/userMessageController";
 
 const AddMessageRouter = Express.Router();
 
 AddMessageRouter.route("/").get(getAllMessages).post(createMessage);
+AddMessageRouter.route("/:id").delete(deleteSingleMessage);
 
 export default AddMessageRouter;
